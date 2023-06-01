0ccf89ad20cafa778253a4a50d0f39eb2e39b272
TJ <tj@tjemail.com>
2023-06-01 15:37:59 +0100

    update prod to ensure release won't override k8s config

--- output.tf
+++ output.tf
@@ -1,6 +1,3 @@
 output "current_oid" {
   value = data.azurerm_client_config.current.object_id
 }
-output "ci_service_endpoint" {
-  value = data.azuread_service_principal.ci_service_principle.object_id
-}
